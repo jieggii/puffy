2f5ce44107683d114305c214f4a4276e11a57f53
jieggii <jieggii.contact@gmail.com>
2021-08-26 17:59:55 +0300

    Remove -f from some rm commands

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Uninstalling puffy..."
 sudo rm -f /usr/bin/puffy
-sudo rm -rif /etc/puffy
-sudo rm -if /etc/systemd/system/puffy.service
+sudo rm -ir /etc/puffy
+sudo rm -i /etc/systemd/system/puffy.service
 echo "Done!"
