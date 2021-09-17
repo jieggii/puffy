91a64e488e58ddd04af5c459a36b4a57abbc95b0
jieggii <jieggii.contact@gmail.com>
2021-09-17 12:45:02 +0300

    Some small fixes

--- install.sh
+++ install.sh
@@ -3,14 +3,15 @@
 echo "Installing puffy!"
 echo "Building puffy..."
 go build src/main.go
+echo ""
 
 echo "Copying some files..."
 sudo mv -iv main /usr/bin/puffy
 sudo mkdir -pv /etc/puffy
 sudo cp -iv config.example.toml /etc/puffy/config.toml
 sudo cp -iv puffy.service /etc/systemd/system/
-
 echo ""
+
 echo "Done!"
 echo "If you don't see any errors above, puffy is likely installed :)"
 echo "Now I suppose you would like to edit the config file (by default it is located at /etc/puffy/config.toml)"
